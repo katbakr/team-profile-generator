8459055fb72b9a4264358f8090b278f782378b73
Katrina Baker <katrinamourine@gmail.com>
2022-12-05 12:54:16 -0800

    adding inquirer questions for employee

--- index.js
+++ index.js
@@ -73,4 +73,103 @@ const addManager = () => {
 
         team.push(manager);
     })
+};
+
+const addEmployee = () => {
+    console.log('Add your first employee!')
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'title',
+            message: 'Choose the title of your employee: ',
+            choices: ['Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of this employee?'
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter the employee name!')
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input', 
+            name: 'id',
+            message: 'Employee ID: ',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter employee ID for the employee!');
+                }
+            }
+        },
+        {
+            type:'input', 
+            name: 'email',
+            message: 'Email: ',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email address for the employee!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Github Username: ',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter the employee Github username!')
+                    return false;
+                }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the intern's school: ",
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the intern's school!")
+                    return false;
+                }
+            }  
+        },
+        {
+            type: 'confirm',
+            name: 'confirmEmployee',
+            message: 'Do you need to add another employee?',
+            default: false,
+        },
+    ])
+    .then(employeeInput => {
+        let {name, id, email, title, github, school, confirmEmployee } = employeeInput;
+        let employee;
+
+        if (role === 'Engineer') {
+            employee = new Engineer (name, id, email, github);
+        } else if (role === 'Intern') {
+            employee = new Intern (name, id, email, school);
+        }
+
+        team.push(employee);
+
+        if (confirmEmployee) {
+            return addEmployee(team);
+        } else {
+            return team;
+        }
+    })
 }
