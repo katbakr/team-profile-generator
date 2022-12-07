a1710c177939d2d22b94e17b4546edb563c9332b
Katrina Baker <katrinamourine@gmail.com>
2022-12-06 21:23:05 -0800

    creating cards for employees and adding them to HTML template

--- src/templateHTML.js
+++ src/templateHTML.js
@@ -1,6 +1,78 @@
+const templateManager = function (manager) {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Manager</h5>
+      <h5 class="card-subtitle mb-2 text-muted">${manager.name}</h5>
+      <p class="id">ID: ${manager.id}</p>
+      <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</p>
+      <p class="office">Office Number: ${manager.office}</p>
+    </div>
+  </div> 
+  `
+}
+
+const templateEngineer = function (engineer) {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Engineer</h5>
+      <h5 class="card-subtitle mb-2 text-muted">${engineer.name}</h5>
+      <p class="id">ID: ${engineer.id}</p>
+      <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</p>
+      <p class="github">GitHub: ${engineer.github}</p>
+    </div>
+  </div>     
+`
+}
+
+const templateIntern= function (intern) {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Intern</h5>
+      <h5 class="card-subtitle mb-2 text-muted">${intern.name}</h5>
+      <p class="id">ID: ${intern.id}</p>
+      <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</p>
+      <p class="school">School: ${intern.school}</p>
+    </div>
+  </div>     
+`
+}
+
+generateHTML = (data) => {
+
+    employeeArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const title = employee.getTitle
+    }
 
 
-const templateTeamPage = function{
+    if (title === 'manager') {
+        const managerData = templateManager(employee)
+        employeeArray.push(managerData);
+    }
+
+    if (title === 'engineer') {
+        const engineerData = templateengineer(employee)
+        employeeArray.push(engineerData);
+    }
+
+    if (title === 'intern') {
+        const internData = templateintern(employee)
+        employeeArray.push(internData);
+    }
+
+
+const employeeData = employeeArray.join('')
+
+const generateTeam = templateTeamPage(employeeData);
+return generateTeam;
+}
+
+const templateTeamPage = function(employeeData){
     return` 
     <!DOCTYPE html>
     <html lang="en">
@@ -11,7 +83,15 @@ const templateTeamPage = function{
         <title>Document</title>
     </head>
     <body>
-        
+    <header>
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">My Team</span>
+      </nav>
+   </header> 
+<div class="cards">
+${employeeData}
+</div>
+
     </body>
     </html> 
     `
